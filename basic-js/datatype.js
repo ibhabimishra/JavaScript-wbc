facd26408176ae2d8a6f10ff22721718ad59a48f
ibhabimishra <ibhabimishra95@gmail.com>
2024-11-02 05:55:47 +0000

    datatype

---
+++ basic-js/datatype.js
@@ -0,0 +1,38 @@
+// let age=34;
+// console.log(age)
+// console.log(typeof(age))
+
+// var a="web bocket"
+// console.log(a)
+// console.log(typeof(a))
+
+// const b=null
+// console.log(b)
+// console.log(typeof(b))
+
+// let mySalary;
+// console.log(mySalary)
+// console.log(typeof(mySalary))
+
+// let y=Symbol("hello")
+// console.log(y)
+// console.log(y.description)
+// console.log(typeof(y))
+
+// let isSalarycredit=false
+// console.log(isSalarycredit)
+// console.log(typeof(isSalarycredit))
+
+// let arr=[34,67,54,67,89,43,23,90]
+// console.log(arr)
+// console.log(arr[2])
+// console.log(arr[7])
+// console.log(typeof(arr))
+
+let moon={
+    "name:"moon mishra",
+    "phone no":97645267,
+    "address":"bbsr",
+    "carrier":"good"
+}
+ console.log(moon)
---
+++ basic-js/variable.js
@@ -0,0 +1,10 @@
+const accountId=1234
+let accountEmail="web.bocket@gmail.com"
+var accountPassword="12345"
+accountCity="bhubaneswar"
+let accountState;
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.log(accountState)
